aa3771e8a23f070b8d89fe13ca4c886bd4b0f0d2
youjin <youjinyeo12@gmail.com>
2025-11-06 16:48:55 +0900

    👷Model 추가

---
+++ src/main/kotlin/kr/ac/kumoh/s20220667/w25w08_thymleaf/model/Song.kt
@@ -0,0 +1,7 @@
+package kr.ac.kumoh.s20220667.w25w08_thymleaf.model
+
+data class Song(
+    var id: Int,
+    var title: String,
+    var singer: String,
+)
---
+++ src/main/kotlin/kr/ac/kumoh/s20220667/w25w08_thymleaf/repository/SongRepository.kt
@@ -0,0 +1,4 @@
+package kr.ac.kumoh.s20220667.w25w08_thymleaf.repository
+
+class SongRepository {
+}
